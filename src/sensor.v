10b80b6202fd2a520d14a8d93bb94c6b94a8c850
Arna Roy <arnaroy@Arnas-Laptop.local>
2024-11-04 02:56:12 -0500

    upate

--- src/sensor.v
+++ src/sensor.v
@@ -1,29 +1,29 @@
 `default_nettype none
+
 module sensor #(parameter N_DELAY = 16) (
     input wire  clk,             // System clock
-    output wire delayed_clk // Time difference (number of transitions)
+    output wire delayed_clk      // Delayed clock output
 );
 
+    (* keep = "true" *) wire [N_DELAY+1:0] w_dly_sig;
+    (* keep = "true" *) wire [N_DELAY+1:0] w_dly_sig_1;
+    (* keep = "true" *) wire [N_DELAY+1:0] w_dly_sig_2;
+    (* keep = "true" *) wire [N_DELAY+1:0] w_dly_sig_3;
+    (* keep = "true" *) wire [N_DELAY+1:0] w_dly_sig_4;
+    (* keep = "true" *) wire [N_DELAY+1:0] w_dly_sig_5;
+    (* keep = "true" *) wire [N_DELAY+1:0] w_dly_sig_6;
+    (* keep = "true" *) wire [N_DELAY+1:0] w_dly_sig_7;
+    (* keep = "true" *) wire [N_DELAY+1:0] w_dly_sig_8;
+    (* keep = "true" *) wire [N_DELAY+1:0] w_dly_sig_9;
+    (* keep = "true" *) wire [N_DELAY+1:0] w_dly_sig_10;
+    (* keep = "true" *) wire [N_DELAY:0] w_dly_sig_n;
 
-(* keep = "true" *) wire [N_DELAY+1:0] w_dly_sig;
-(* keep = "true" *) wire [N_DELAY+1:0] w_dly_sig_1;
-(* keep = "true" *) wire [N_DELAY+1:0] w_dly_sig_2;
-(* keep = "true" *) wire [N_DELAY+1:0] w_dly_sig_3;
-(* keep = "true" *) wire [N_DELAY+1:0] w_dly_sig_4;
-(* keep = "true" *) wire [N_DELAY+1:0] w_dly_sig_5;
-(* keep = "true" *) wire [N_DELAY+1:0] w_dly_sig_6;
-(* keep = "true" *) wire [N_DELAY+1:0] w_dly_sig_7;
-(* keep = "true" *) wire [N_DELAY+1:0] w_dly_sig_8;
-(* keep = "true" *) wire [N_DELAY+1:0] w_dly_sig_9;
-(* keep = "true" *) wire [N_DELAY+1:0] w_dly_sig_10;
-(* keep = "true" *) wire [N_DELAY:0] w_dly_sig_n;
+    assign w_dly_sig[0] = clk;
 
-assign w_dly_sig[0] = clk;
-
-genvar i;
-generate
-    for (i = 0; i <= N_DELAY; i = i + 1) begin : g_dly_chain_even
-        (* keep = "true" *) cinv dly_stg1 (.a(w_dly_sig[i]), .q(w_dly_sig_1[i]));
+    genvar i;
+    generate
+        for (i = 0; i <= N_DELAY; i = i + 1) begin : g_dly_chain_even
+            (* keep = "true" *) cinv dly_stg1 (.a(w_dly_sig[i]), .q(w_dly_sig_1[i]));
             (* keep = "true" *) cinv dly_stg2 (.a(w_dly_sig_1[i]), .q(w_dly_sig_2[i]));
             (* keep = "true" *) cinv dly_stg3 (.a(w_dly_sig_2[i]), .q(w_dly_sig_3[i]));
             (* keep = "true" *) cinv dly_stg4 (.a(w_dly_sig_3[i]), .q(w_dly_sig_4[i]));
@@ -33,14 +33,12 @@ generate
             (* keep = "true" *) cinv dly_stg8 (.a(w_dly_sig_7[i]), .q(w_dly_sig_8[i]));
             (* keep = "true" *) cinv dly_stg9 (.a(w_dly_sig_8[i]), .q(w_dly_sig_9[i]));
             (* keep = "true" *) cinv dly_stg10 (.a(w_dly_sig_9[i]), .q(w_dly_sig_10[i]));
-
-            // After passing through 10 stages of inverters, proceed with the final inverter stage
             (* keep = "true" *) cinv dly_stg11 (.a(w_dly_sig_10[i]), .q(w_dly_sig_n[i]));
-
-            // The final inverter chain back to w_dly_sig for the next iteration
             (* keep = "true" *) cinv dly_stg12 (.a(w_dly_sig_n[i]), .q(w_dly_sig[i+1]));
-    end
+        end
+    endgenerate
 
-assign delayed_clk = w_dly_sig[N_DELAY];
+    assign delayed_clk = w_dly_sig[N_DELAY];
 
 endmodule
+
